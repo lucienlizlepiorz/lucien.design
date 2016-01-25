4249e4f6a724cb0d805bec243e1c0c43cb03527f
lucienlizlepiorz <Lucien@LLL-A1466.home>
2016-01-25 10:18:00 -0500

    cleaned link styles

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,8 +1,8 @@
 	<div id="footer">
 		<h3>
 			<span class="mono-regular">
-				<a href="index.php"><span class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "index.php" || basename($_SERVER["SCRIPT_NAME"]) != "about.php") {echo "current-page";} else {echo "";} ?>">work</span></a>
-				 :: <a href="about.php"><span class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "about.php") {echo "current-page";} else {echo "";} ?>">about</span></a>
+				<a href="index.php" class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "index.php" || basename($_SERVER["SCRIPT_NAME"]) != "about.php") {echo "current-page";} else {echo "";} ?>">work</a>
+				 :: <a href="about.php" class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "about.php") {echo "current-page";} else {echo "";} ?>">about</a>
 				 :: say <a href="mailto:howdy@lucien.design" target="_blank">howdy@lucien.design</a>
 				 :: <a href="https://www.github.com/lucienlizlepiorz" target="_blank">GitHub</a>
 			</span>
