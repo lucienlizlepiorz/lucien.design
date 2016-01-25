db3eb0371640fa1a667522772989f1e27bd8a68d
lucienlizlepiorz <Lucien@LLL-A1466.home>
2016-01-25 14:24:49 -0500

    file structure correction

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -30,7 +30,7 @@
 
 	<div id="name">
 		<h1>
-			<a href="index.php" class="no-underline"><span class="mono-bold">Lucien</span> Liz-Lepiorz</a>
+			<a href="/" class="no-underline"><span class="mono-bold">Lucien</span> Liz-Lepiorz</a>
 		</h1>
 	</div>
 	
--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,7 +1,7 @@
 <div id="navigation">
 	<h3>
 		<span class="mono-regular">
-			<a href="index.php" class="<?php if(basename($_SERVER[REQUEST_URI]) == "work" || basename($_SERVER[REQUEST_URI]) == "index.php" || basename($_SERVER[REQUEST_URI]) == "lucien.design") {echo "current-page";} else {echo "";} ?>">work</a>
+			<a href="/" class="<?php if(basename($_SERVER[REQUEST_URI]) == "" || basename($_SERVER[REQUEST_URI]) == "work") {echo "current-page";} else {echo "";} ?>">work</a>
 			 :: <a href="about" class="<?php if(basename($_SERVER[REQUEST_URI]) == "about") {echo "current-page";} else {echo "";} ?>">about</a>
 			</br>
 			say <a href="mailto:howdy@lucien.design" target="_blank">howdy@lucien.design</a>
