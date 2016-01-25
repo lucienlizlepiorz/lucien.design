d2542b430edb05d37e8b8d50cd927bd3ff7025d7
lucienlizlepiorz <Lucien@LLL-A1466.home>
2016-01-25 10:07:09 -0500

    updated current page css

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,8 +1,8 @@
 	<div id="footer">
 		<h3>
 			<span class="mono-regular">
-				<span class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "index.php" || basename($_SERVER["SCRIPT_NAME"]) != "about.php") {echo "current-page";} else {echo "";} ?>">work</span>
-				 :: <span class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "about.php") {echo "current-page";} else {echo "";} ?>">about</span> 
+				<a href="index.php"><span class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "index.php" || basename($_SERVER["SCRIPT_NAME"]) != "about.php") {echo "current-page";} else {echo "";} ?>">work</span></a>
+				 :: <a href="about.php"><span class="<?php if(basename($_SERVER["SCRIPT_NAME"]) == "about.php") {echo "current-page";} else {echo "";} ?>">about</span></a>
 				 :: say <a href="mailto:howdy@lucien.design" target="_blank">howdy@lucien.design</a>
 				 :: <a href="https://www.github.com/lucienlizlepiorz" target="_blank">GitHub</a>
 			</span>
