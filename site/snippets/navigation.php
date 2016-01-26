f04f7d582d0161e0c03c783c5eee668a9223c6cd
lucienlizlepiorz <Lucien@LLL-A1466.home>
2016-01-26 09:53:30 -0500

    updated nav, styles

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,11 +1,4 @@
 <div id="navigation">
-	<h3>
-		<span class="mono-regular">
-			<a href="/" class="<?php if(basename($_SERVER[REQUEST_URI]) == "" || basename($_SERVER[REQUEST_URI]) == "work") {echo "current-page";} else {echo "";} ?>">work</a>
-			 :: <a href="about" class="<?php if(basename($_SERVER[REQUEST_URI]) == "about") {echo "current-page";} else {echo "";} ?>">about</a>
-			</br>
-			say <a href="mailto:howdy@lucien.design" target="_blank">howdy@lucien.design</a>
-			 :: <a href="https://www.github.com/lucienlizlepiorz" target="_blank">GitHub</a>
-		</span>
-	</h3>
+	<h3><a href="/" class="<?php if(basename($_SERVER[REQUEST_URI]) != "about") {echo "current-page";} else {echo "";} ?>">work</a> :: <a href="about" class="<?php if(basename($_SERVER[REQUEST_URI]) == "about") {echo "current-page";} else {echo "";} ?>">about</a></h3>
+	<h3>say <a href="mailto:howdy@lucien.design" target="_blank">howdy@lucien.design</a> :: <a href="https://www.github.com/lucienlizlepiorz" target="_blank">GitHub</a></h3>
 </div>
