9e456272be05cd4a2ce0eced7b3ceca7eeb8ee0c
lucienlizlepiorz <Lucien@192.168.1.15>
2016-02-01 01:20:25 -0500

    fixed nav link

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,4 +1,4 @@
 <div id="navigation">
-	<h3><a href="/" class="<?php if(basename($_SERVER[REQUEST_URI]) != "about") {echo "current-page";} else {echo "";} ?>">work</a> :: <a href="about" class="<?php if(basename($_SERVER[REQUEST_URI]) == "about") {echo "current-page";} else {echo "";} ?>">about</a></h3>
+	<h3><a href="/" class="<?php if(basename($_SERVER[REQUEST_URI]) != "about") {echo "current-page";} else {echo "";} ?>">work</a> :: <a href="/about" class="<?php if(basename($_SERVER[REQUEST_URI]) == "about") {echo "current-page";} else {echo "";} ?>">about</a></h3>
 	<h3>say <a href="mailto:howdy@lucien.design" target="_blank">howdy@lucien.design</a> :: <a href="https://www.github.com/lucienlizlepiorz" target="_blank">GitHub</a></h3>
 </div>
