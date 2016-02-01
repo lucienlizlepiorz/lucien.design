8df9aa7c044c3c00b2ac17f9f3ae02b0276f1450
lucienlizlepiorz <Lucien@LLL-A1466.home>
2016-02-01 15:39:35 -0500

    finished project template

---
+++ site/tags/project-image.php
@@ -0,0 +1,23 @@
+<?php
+
+kirbytext::$tags["project-image"] = array(
+  "attr" => array(
+    "description"
+  ),
+  "html" => function($tag) {
+  
+  	$file 			= $tag->file($tag->attr("project-image"));
+  	$url 			= $file->url();
+  	$description 	= $tag->attr("description");
+    
+    if ($description != "") {
+    	$caption = "<div class='project-image-caption'><h3>".$description."</h3></div>";
+    } else {
+    	$caption = NULL;
+    }
+            
+    return "<p><div class='project-image-container'><img class='project-image' src='".$url."' alt='".$description."'>".$caption."</div></p>";
+  }
+);
+
+?>
--- site/templates/project.php
+++ site/templates/project.php
@@ -6,10 +6,4 @@
 
 <?php echo $page->text()->kirbytext() ?>
 
-<?php foreach($page->images() as $image): ?>
-	<img class="project-image" src="<?php echo $image->url() ?>" alt="<?php echo $image->caption() ?>">
-		<?php if($image->caption() != "") {echo "<div class=\"project-image-caption\"><h3>"; echo $image->caption(); echo "</h3></div>";} ?>
-	</img>
-<?php endforeach ?>
-
 <?php snippet("footer") ?>
