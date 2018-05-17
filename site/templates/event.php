cf3928ed81e6570b7d1a12c1558ca4ef6d9971be
Lucien Liz-Lepiorz <lucien@lucien.design>
2018-05-16 20:05:53 -0500

    added support for more timeline events

---
+++ site/templates/event.php
@@ -0,0 +1,6 @@
+<?php 
+
+// go to index
+go("/");
+
+?>
--- site/templates/work.php
+++ site/templates/work.php
@@ -8,14 +8,22 @@
 
 <ul id="content-list">
 
-	<?php foreach(page('work')->children()->visible()->flip() as $project): ?>
-	<li>
-		<div class="content-title-wrapper">
-			<a class="content-title mono-bold" href="<?php echo $project->url() ?>"><?php echo smartypants($project->title()) ?></a>
-			<h3 class="content-metadata secondary"><?php echo smartypants($project->keywords()) ?></h3>
+	<?php foreach(page('work')->children()->visible()->flip() as $timelineEvent): ?>
+	<li class="timeline">
+
+		<?php if ($timelineEvent->intendedTemplate() == "project"): ?>
+		<div class="content-title-wrapper event">
+			<a class="content-title mono-bold" href="<?php echo $timelineEvent->url() ?>"><?php echo smartypants($timelineEvent->title()) ?></a>
+			<h3 class="content-metadata secondary"><?php echo smartypants($timelineEvent->keywords()) ?></h3>
 		</div>
-		<a class="content-thumb" href="<?php echo $project->url() ?>"><img src="<?php echo $project->image("thumb.png")->url() ?>" alt="<?php echo smartypants($project->title()) ?>"></img></a>
+		<a class="content-thumb" href="<?php echo $timelineEvent->url() ?>"><img src="<?php echo $timelineEvent->image("thumb.png")->url() ?>" alt="<?php echo smartypants($timelineEvent->title()) ?>"></img></a>
 		<div class="clear"></div>
+		<?php endif ?>
+
+		<?php if ($timelineEvent->intendedTemplate() == "event"): ?>
+		<p class="event"><?php echo smartypants($timelineEvent->text()->kirbytext()) ?></p>
+		<?php endif ?>
+		
   	</li>
   	<?php endforeach ?>
   
