f94145b442d3a2d50a9b7253b0be78b21c898372
Lucien Liz-Lepiorz <lucien@lucien.design>
2020-03-04 11:04:46 -0600

    updated file structure, summary

--- site/snippets/navigation.php
+++ site/snippets/navigation.php
@@ -1,4 +1,4 @@
 <div id="navigation">
-	<h3><a href="/assets/pdf/Lucien-Liz-Lepiorz-CV.pdf" class="cv">view curriculum vitae ></a></h3>
+	<h3><a href="/assets/pdf/Lucien-Liz-Lepiorz-CV.pdf" class="cv">view my CV ></a></h3>
 	<h3>say <a href="mailto:howdy@lucien.design">howdy@lucien.design</a> :: <a href="https://www.github.com/lucienlizlepiorz">GitHub</a></h3>
 </div>
