11da3c17b0adf408a2c5e6056cf6e0bc765c1072
Lucien Liz-Lepiorz <lucien.lizlepiorz@gmail.com>
2020-06-29 19:16:57 -0500

    finished fixing spacing

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ require(__DIR__ . DS . 'kirby' . DS . 'bootstrap.php');
 
 // check for a custom site.php
 if(file_exists(__DIR__ . DS . 'site.php')) {
-  require(__DIR__ . DS . 'site.php');
+	require(__DIR__ . DS . 'site.php');
 } else {
-  $kirby = kirby();
+	$kirby = kirby();
 }
 
 // render
--- site/blueprints/site.php
+++ site/blueprints/site.php
@@ -3,18 +3,18 @@
 title: Site
 pages: default
 fields:
-  title:
-    label: Title
-    type:  text
-  author:
-    label: Author
-    type:  text
-  description:
-    label: Description
-    type:  textarea
-  keywords:
-    label: Keywords
-    type:  tags
-  copyright:
-    label: Copyright
-    type:  textarea
+	title:
+		label: Title
+		type:  text
+	author:
+		label: Author
+		type:  text
+	description:
+		label: Description
+		type:  textarea
+	keywords:
+		label: Keywords
+		type:  tags
+	copyright:
+		label: Copyright
+		type:  textarea
